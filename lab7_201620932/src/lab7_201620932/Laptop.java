397240f8ff6bb95c2d7065701f6a0fc0e879f40f
Sahng <Henry.Sahng@HenrySahng>
2017-04-30 20:15:45 +0900

    dsa

---
+++ lab7_201620932/src/lab7_201620932/Laptop.java
@@ -0,0 +1,56 @@
+package lab7_201620932;
+
+public class Laptop extends Product implements Networked, DataStorage {
+
+	double totalCapacity;
+	double usedCapacity;
+	
+	Laptop() {}
+
+	@Override
+	public double getFreeCapacity() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void format() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isConnected() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double maxSpeed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+	public double getTotalCapacity() {
+		return totalCapacity;
+	}
+
+	public void setTotalCapacity(double totalCapacity) {
+		this.totalCapacity = totalCapacity;
+	}
+
+	public double getUsedCapacity() {
+		return usedCapacity;
+	}
+
+	public void setUsedCapacity(double usedCapacity) {
+		this.usedCapacity = usedCapacity;
+	};
+}
